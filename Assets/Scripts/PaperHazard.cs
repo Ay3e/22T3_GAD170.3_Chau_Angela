3fdbfd71bceb9e42d3977a03f862012baaa280fd
Ay3e <79293475+Ay3e@users.noreply.github.com>
2022-11-23 12:06:01 +1100

    Constructed the office floor plan

--- Assets/Scripts/PaperHazard.cs
+++ Assets/Scripts/PaperHazard.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace AngelaChau
 {
+    
     public class PaperHazard : MonoBehaviour
     {
+        public GameObject playerCharacter;
         private void OnCollisionEnter(Collision collision)
         {
             Debug.Log("PlayerCharacter Died");
             //if player touches paper
             //restart scene
+            playerCharacter.GetComponent<Transform>().transform.position = new Vector3(-15, 1, -20);
         }
+
+        //if(Input.GetKeyDown(KeyCode.E))
+        //{
+        //  DO THE THING
+        //  1. set UI gameobject to ACTIVE
+        //  2. if the player presses the button 
+        //}
     }
 }
 
